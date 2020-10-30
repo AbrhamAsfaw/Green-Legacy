c5cf7bda9ff73cba02448cc0745b2d03fabee0ba
ABRHAMASFAW2018 <abrhamasfaw2018@gmail.com>
2020-10-30 18:26:11 +0300

    second commit

--- lib/main.dart
+++ lib/main.dart
@@ -10,12 +10,16 @@ void main() {
   
 }
 
+
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: SignUpSection(),
-      routes: {LandingScreen.id: (context) => LandingScreen()},
+      routes: {
+        LandingScreen.id: (context) => LandingScreen(),
+        LoginSection.id: (context) => LoginSection(),
+      },
     );
   }
 }
@@ -26,6 +30,15 @@ class SignUpSection extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    checkToken() async {
+      SharedPreferences prefs = await SharedPreferences.getInstance();
+      String token = prefs.getString("token");
+      if (token != null) {
+        Navigator.pushNamed(context, LandingScreen.id);
+      }
+    }
+
+    checkToken();
     return CupertinoPageScaffold(
         navigationBar: CupertinoNavigationBar(
           automaticallyImplyLeading: false,
@@ -48,22 +61,94 @@ class SignUpSection extends StatelessWidget {
                     password = value;
                   })),
           FlatButton.icon(
-              onPressed: () async {
-                signup(email, password);
-                SharedPreferences prefs = await SharedPreferences.getInstance();
-                String token = prefs.getString("token");
-                if (token != null) {
-                  Navigator.pushNamed(context, LandingScreen.id);
-                }
+            onPressed: () async {
+              signup(email, password);
+              SharedPreferences prefs = await SharedPreferences.getInstance();
+              String token = prefs.getString("token");
+              if (token != null) {
+                Navigator.pushNamed(context, LandingScreen.id);
+              }
+            },
+            icon: Icon(Icons.save),
+            label: Text("Sign UP"),
+          ),
+          FlatButton(
+              onPressed: () {
+                Navigator.pushNamed(context, LoginSection.id);
               },
-              icon: Icon(Icons.save),
-              label: Text("Sign Up"))
+              child: Text("login")),
         ])));
   }
 }
 
 signup(email, password) async {
-  var url = "http://10.0.2.2:5000/signup"; // Android
+  var url = "http://10.0.2.2:5000/signup"; // iOS
+  final http.Response response = await http.post(
+    url,
+    headers: <String, String>{
+      'Content-Type': 'application/json; charset=UTF-8',
+    },
+    body: jsonEncode(<String, String>{
+      'email': email,
+      'password': password,
+    }),
+  );
+  print(response.body);
+  SharedPreferences prefs = await SharedPreferences.getInstance();
+  var parse = jsonDecode(response.body);
+
+  await prefs.setString('token', parse["token"]);
+}
+
+class LoginSection extends StatelessWidget {
+  static const String id = "LoginSection";
+  var email;
+  var password;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: CupertinoPageScaffold(
+          navigationBar: CupertinoNavigationBar(
+            automaticallyImplyLeading: false,
+          ),
+          child: SafeArea(
+              child: ListView(padding: const EdgeInsets.all(16), children: [
+            Padding(
+                padding: const EdgeInsets.symmetric(vertical: 8),
+                child: CupertinoTextField(
+                    placeholder: "Email",
+                    onChanged: (value) {
+                      email = value;
+                    })),
+            Padding(
+                padding: const EdgeInsets.symmetric(vertical: 8),
+                child: CupertinoTextField(
+                    placeholder: "Password",
+                    obscureText: true,
+                    onChanged: (value) {
+                      password = value;
+                    })),
+            FlatButton.icon(
+                onPressed: () async {
+                  login(email, password);
+                  SharedPreferences prefs =
+                      await SharedPreferences.getInstance();
+                  String token = prefs.getString("token");
+                  if (token != null) {
+                    Navigator.pushNamed(context, LandingScreen.id);
+                  }
+                },
+                icon: Icon(Icons.save),
+                label: Text("Login"))
+          ]))),
+    );
+  }
+}
+
+login(email, password) async {
+  var url = "http://10.0.2.2:5000/login"; // Android
   final http.Response response = await http.post(
     url,
     headers: <String, String>{
@@ -85,6 +170,21 @@ class LandingScreen extends StatelessWidget {
   static const String id = "LandingScreen";
   @override
   Widget build(BuildContext context) {
-    return Text("Welcome to Green Legacy");
+    return Scaffold(
+        body: Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        Center(child: Text("Welcome to the Landing Screen")),
+        FlatButton.icon(
+            onPressed: () async {
+              SharedPreferences prefs = await SharedPreferences.getInstance();
+              await prefs.setString('token', null);
+              Navigator.pushNamed(context, LoginSection.id);
+            },
+            icon: Icon(Icons.send),
+            label: Text("Logout"))
+      ],
+    ));
   }
 }
+
